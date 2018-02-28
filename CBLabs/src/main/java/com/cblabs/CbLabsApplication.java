6e9f74a8578911b2446dc27e006f72ed0f566072
liuw <namingliu@163.com>
2018-02-28 12:53:08 +0800

    add spring boot project

---
+++ CBLabs/src/main/java/com/cblabs/CbLabsApplication.java
@@ -0,0 +1,12 @@
+package com.cblabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbLabsApplication.class, args);
+	}
+}
